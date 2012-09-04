10b0498c2ba144e786254f64051fa71c6eaf276b
Chris Hansen <moosekid5@gmail.com>
2012-09-03 18:46:57 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResponseMonster::Application.config.secret_token = '16756de2d741351c49fc039d6e32c3f2b81add18690a7edf4bdf608822f48218013670de8c86f2c435e86d18f2df672d90405a80ba68a490e4c510a842e07015'
