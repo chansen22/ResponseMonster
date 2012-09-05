3936c7876cb3c182230154400125dfb2cc58a785
Chris Hansen <moosekid5@gmail.com>
2012-09-05 07:10:20 -0700

    Removed extra comments from pre generated files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,36 @@
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.json
+  
   def index
     @users = User.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html 
       format.json { render json: @users }
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @user = User.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html 
       format.json { render json: @user }
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html 
       format.json { render json: @user }
     end
   end
-
-  # GET /users/1/edit
+  
   def edit
     @user = User.find(params[:id])
   end
-
-  # POST /users
-  # POST /users.json
+  
   def create
     @user = User.new(params[:user])
 
@@ -52,9 +44,7 @@ class UsersController < ApplicationController
       end
     end
   end
-
-  # PUT /users/1
-  # PUT /users/1.json
+  
   def update
     @user = User.find(params[:id])
 
@@ -68,9 +58,7 @@ class UsersController < ApplicationController
       end
     end
   end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
+  
   def destroy
     @user = User.find(params[:id])
     @user.destroy
