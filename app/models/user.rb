98d0ecec14097ab364543be0cc5a90a7b6b1823a
Chris Hansen <moosekid5@gmail.com>
2012-09-24 08:49:31 -0700

    removed lengthy email regexp and just turned it into requiring an @ symbol

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates(:first_name, :last_name, presence: true, length: { maximum: 50 })
   validates(:password, presence: true, length: { minimum: 6 })
   validates(:password_confirmation, presence: true)
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /@/i
   validates(:email, presence: true, 
             uniqueness: { case_sensitive: false }, 
             format: { with: VALID_EMAIL_REGEX })
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -86,7 +86,7 @@ describe User do
   describe "when an email is invalid" do
     describe "when email format is invalid" do
       it "should be invalid" do
-        addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+        addresses = %w[userfoo,com user_at_foo.org example.userfoo. foobar_baz.com foobar+baz.com]
         addresses.each do |invalid_address|
           @user.email = invalid_address
           @user.should_not be_valid
