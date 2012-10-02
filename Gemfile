aeef676690fa15d9bf57a48985c37945f88e6087
Chris Hansen <moosekid5@gmail.com>
2012-10-01 17:55:56 -0700

    Added nested forms for ease of use to add new polls to surveys

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.8'
 gem 'haml'
 gem 'bcrypt-ruby'
 gem 'bootstrap-sass', '2.0.4'
+gem 'nested_form'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,8 @@
 class Survey < ActiveRecord::Base
   belongs_to      :course
   has_many        :polls
-  attr_accessible :end_time, :name, :start_time, :is_active
+  attr_accessible :end_time, :name, :start_time, :is_active, :polls_attributes
+  accepts_nested_attributes_for :polls, allow_destroy: true
 
   def toggle_survey(survey)
     survey.toggle!(:is_active)
