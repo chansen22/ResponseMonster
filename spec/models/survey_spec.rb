ba132726600fd27fa1f906773e6ff961d7635d68
Chris Hansen <moosekid5@gmail.com>
2012-11-05 14:47:11 -0800

    Fixed test that should no longer past

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -19,11 +19,6 @@ describe Survey do
   it { should be_valid }
 
   describe "when a name is valid" do
-    describe "when a name is not present" do
-      before { @survey.name = "" }
-      it { should be_valid }
-    end
-
     describe "when a name is present" do
       it { should be_valid }
     end
