6378322b67a25baff5a12f15d161720ca4543eba
Alan Braithwaite <abraithwaite@mail.csuchico.edu>
2012-12-12 00:20:33 -0800

    Fixed bug with clicking submit multiple times on assessments/create method

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -2,7 +2,7 @@ class AssessmentsController < ApplicationController
   require 'debugger'
   before_filter :authenticate
   before_filter(only: [:new]) { |controller| controller.check_activated(Survey.find(params[:survey_id])) }
-  before_filter(only: [:new, :create]) { |controller| controller.check_attempts(Survey.find(params[:survey_id])) }
+  before_filter(only: [:new]) { |controller| controller.check_attempts(Survey.find(params[:survey_id])) }
   before_filter(only: [:index]) { |controller| controller.check_permissions(Course.find(params[:course_id])) }
 
   def show
@@ -20,6 +20,10 @@ class AssessmentsController < ApplicationController
   def create
     @survey = Survey.find(params[:survey_id])
     @course = Course.find(params[:course_id])
+    if current_user.assessments.where(survey_id: @survey).length >= @survey.attempts_allowed
+      redirect_to course_path(@course), notice: "You cannot take this quiz anymore times."
+      return
+    end
     @assessment = current_user.assessments.new(params[:assessment])
     @assessment.times_submitted = current_user.assessments.where(survey_id: @survey).length + 1
     @assessment.survey = @survey
@@ -28,7 +32,7 @@ class AssessmentsController < ApplicationController
       @assessment.save
       redirect_to course_path(@course), notice: "Assessment was successfully submitted!"
     else
-      render new_course_survey_path(@course), notice: "There were some errors submitting your Assessment."
+      redirect_to course_path(@course), notice: "There were some errors submitting your Assessment."
     end
   end
 
